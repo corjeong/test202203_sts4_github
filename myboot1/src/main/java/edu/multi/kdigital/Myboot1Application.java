8e6aabd1b7cd1aff9024fd572d00c062c0f59d2d
corjeong <88470821+corjeong@users.noreply.github.com>
2022-03-21 15:24:58 +0900

    Update Myboot1Application.java

--- myboot1/src/main/java/edu/multi/kdigital/Myboot1Application.java
+++ myboot1/src/main/java/edu/multi/kdigital/Myboot1Application.java
@@ -9,6 +9,7 @@ public class Myboot1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Myboot1Application.class, args);
 		System.out.println("깃헙 push 용도");
+		System.out.println("원격저장소 추가 라인");
 	}
 
 }
